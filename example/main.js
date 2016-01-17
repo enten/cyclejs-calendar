5ae9a11eda4893a7a048f475f433c9e334e37a83
Steven Enten <steven@enten.fr>
2016-01-17 21:24:11 +0100

    Update main.js bundle

--- example/main.js
+++ example/main.js
@@ -42,7 +42,9 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { 'default': obj };
+}
 
 var _rx = require('rx');
 
